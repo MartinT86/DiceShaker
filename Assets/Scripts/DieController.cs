d6e4239f73c14a844e45c8643ffb986af584a568
m <martin.tierney86@hotmail.co.uk>
2017-03-12 14:49:38 +0000

    initial

---
+++ Assets/Scripts/DieController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        //float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(Input.acceleration.x, Input.acceleration.y, -Input.acceleration.z);
+        rb.AddForce(movement * speed);
+    }
+}
