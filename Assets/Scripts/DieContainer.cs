a0a2ea9da2377cc8f1d08dd56af3ed0f9a6b83f8
m <martin.tierney86@hotmail.co.uk>
2017-03-15 23:09:43 +0000

    add dice script

---
+++ Assets/Scripts/DieContainer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieContainer : MonoBehaviour
+{
+    public GameObject dieOne;
+    public GameObject dieTwo;
+    public GameObject dieThree;
+    public GameObject dieFour;
+    public GameObject dieFive;
+
+    private GameObject[] dice;
+    private int dieCount;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    dieCount = 1;
+		dice = new GameObject[]
+		{
+		    dieOne,
+            dieTwo,
+            dieThree,
+            dieFour,
+            dieFive
+		};
+	}
+
+    public void AddDie()
+    {
+        if (dieCount > 0 && dieCount < 6)
+        {
+            dice[dieCount - 1].SetActive(true);
+            dieCount = dieCount + 1;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
